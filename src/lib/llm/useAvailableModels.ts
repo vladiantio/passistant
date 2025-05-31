bfe61cb0dc3c4cd39853a70ad2273d187656a5e2
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-05-31 18:45:54 -0400

    refactor(llm): move asynchronous initialization logic into separate function for dynamic  import to @mlc-ai/web-llm

--- src/lib/llm/useAvailableModels.ts
+++ src/lib/llm/useAvailableModels.ts
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react"
-import { prebuiltAppConfig } from "@mlc-ai/web-llm"
 
 export function useAvailableModels() {
   const [models, setModels] = useState<string[]>([])
 
   useEffect(() => {
-    const modelList = prebuiltAppConfig.model_list.map((m) => m.model_id)
-    setModels(modelList)
+    const init = async () => {
+      const { prebuiltAppConfig } = await import('@mlc-ai/web-llm')
+      const modelList = prebuiltAppConfig.model_list.map((m) => m.model_id)
+      setModels(modelList)
+    }
+    init()
   }, [])
 
   return models
--- src/lib/llm/useMLCEngine.ts
+++ src/lib/llm/useMLCEngine.ts
@@ -1,16 +1,20 @@
 import { useEffect, useState } from 'react'
-import { MLCEngine, type InitProgressReport } from '@mlc-ai/web-llm'
+import type { MLCEngine, InitProgressReport } from '@mlc-ai/web-llm'
 
 export function useMLCEngine() {
   const [engine, setEngine] = useState<MLCEngine | null>(null)
   const [initProgressReport, setInitProgressReport] = useState<InitProgressReport | null>(null)
 
   useEffect(() => {
-    const engine = new MLCEngine()
-    engine.setInitProgressCallback((report) => {
-      setInitProgressReport(report)
-    })
-    setEngine(engine)
+    const init = async () => {
+      const { MLCEngine } = await import('@mlc-ai/web-llm')
+      const engine = new MLCEngine()
+      engine.setInitProgressCallback((report) => {
+        setInitProgressReport(report)
+      })
+      setEngine(engine)
+    }
+    init()
   }, [])
 
   return { engine, initProgressReport }
