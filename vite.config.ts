4fb2a3211317fa6d66eeb6a6dbc3de2ea74fe1ba
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-05-31 19:53:58 -0400

    feat: integrate shadcn
    - Add Tailwind CSS configuration and utility functions, update TypeScript paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
@@ -10,5 +11,10 @@ export default defineConfig({
   ],
   build: {
     target: 'esnext',
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 })
