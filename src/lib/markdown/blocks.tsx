567794cd212f60f2137e81109ee7531896a69351
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-06-03 02:04:14 -0400

    feat(markdown): enhance PasswordBlock with copy to clipboard functionality and improve layout

--- src/lib/markdown/blocks.tsx
+++ src/lib/markdown/blocks.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/ui/button"
+import { copyToClipboard } from "@/utils/clipboard";
+import { Check, Copy } from "lucide-react"
+import { useState } from "react";
+
 export function LoadingBlock() {
   return (
     <span style={{ fontWeight: 'bold', color: 'blue' }}>Loading...</span>
@@ -14,7 +19,43 @@ export function ThinkBlock(props: React.PropsWithChildren) {
 }
 
 export function PasswordBlock(props: React.PropsWithChildren) {
+  const [copied, setCopied] = useState(false);
+
+  const doCopyToClipboard = async () => {
+    if (!props.children) return
+    const success = await copyToClipboard(props.children?.toString());
+    setCopied(success);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
   return (
-    <span style={{ fontWeight: 'bold', color: 'blue' }}>{props.children}</span>
+    <span className="bg-input/40 pl-2 pr-0.5 py-0.5 text-sm rounded-md inline-flex items-center gap-2 font-mono">
+      {props.children ? (
+        <>
+          <span>{props.children}</span>
+          <Button
+            variant="outline"
+            size="icon"
+            className="relative size-5 border-0"
+            onClick={doCopyToClipboard}
+            aria-label={copied ? "Copied" : "Copy to clipboard"}
+          >
+            <span className="sr-only">{copied ? "Copied" : "Copy"}</span>
+            <Copy
+              className={`size-3 transition-all ${
+                copied ? "scale-0" : "scale-100"
+              }`}
+            />
+            <Check
+              className={`absolute inset-0 m-auto size-3 transition-all ${
+                copied ? "scale-100" : "scale-0"
+              }`}
+            />
+          </Button>
+        </>
+      ) : (
+        <span className="mr-2">&lt;pass&gt;</span>
+      )}
+    </span>
   )
 }
