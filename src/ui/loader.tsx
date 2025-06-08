09d06943fde8d62b3097b6c25c83520cd2c5339a
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-06-08 12:45:13 -0400

    fix(ui): add fill attribute to CircleLoader for consistent styling

--- src/ui/loader.tsx
+++ src/ui/loader.tsx
@@ -12,6 +12,7 @@ export function CircleLoader({
       height={size}
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
     >
       <path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z" transform="translate(12, 12) scale(0)">
         <animateTransform id="spinner_g88x" begin="0;spinner_yOmw.begin+0.6s" attributeName="transform" calcMode="spline" type="translate" dur="1.2s" values="12 12;0 0" keySplines=".52,.6,.25,.99"/>
