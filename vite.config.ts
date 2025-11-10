ded64cf4836326587acf345642f3a2578bf2abb6
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-11-10 02:45:10 -0300

    refactor(i18n): remove Lingui configuration and dependencies, migrate to i18next for localization

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import { lingui } from '@lingui/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({ plugins: [["@lingui/swc-plugin", {}]] }),
-    lingui(),
+    react(),
     tailwindcss(),
   ],
   build: {
